5e5316d6d1cbbab4686da0fc4feaf158bad593d0
Ignacio Ruben Ortuño Albarran <ignacioruben7@gmail.com>
2016-03-29 16:53:48 -0600

    Añadiendo la maquina de estados al blog

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,63 @@
+class ArticlesController < ApplicationController
+	before_action :authenticate_user!, except: [:show,:index]
+	before_action :set_article, except: [:index,:new,:create]
+	before_action :authenticate_autor!, only: [:new,:create,:update]
+	before_action :authenticate_admin!, only: [:destroy]
+	#GET /articles
+	def index
+		@articles = Article.all
+	end
+	#GET /articles:id
+	def show
+		@article.update_visits_count
+		@comment = Comment.new
+	end
+	#GET /articles/new
+	def new
+		@article = Article.new
+		@categories = Category.all
+	end
+
+	def edit
+	end
+	#POST /articles
+	def create
+		@article = current_user.articles.new(article_params)
+		@article.categories = params[:categories]
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	#PUT /articles/:id
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def validate_user
+		redirect_to new_user_session_path, notice: "Tienes que iniciar sesion primero"
+	end
+
+	def article_params
+		params.require(:article).permit(:title,:body,:cover,:categories)
+	end
+
+end

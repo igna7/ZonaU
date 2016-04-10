1d7233716296542930010ba65b9d4a5dd9d7fad9
Ignacio Ruben Ortuño Albarran <ignacioruben7@gmail.com>
2016-04-10 17:59:37 -0500

    Añadiendo funcion para que solo el que cree su comentario lo pueda borrar

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 	include AASM
 
 	belongs_to :user
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	has_many :has_categories, dependent: :destroy
 	has_many :categories, through: :has_categories
 

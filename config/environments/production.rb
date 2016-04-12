0a9c74a9fc323c98f6c879d2de59e90edb2a4f50
Ignacio Ruben Ortuño Albarran <ignacioruben7@gmail.com>
2016-04-11 20:04:51 -0500

    Quitando credenciales

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # config/environments/production.rb
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['zonauniversitaria'],
-      :access_key_id => ENV['AKIAIVATA5YPZPPVUDBQ'],
-      :secret_access_key => ENV['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']
-    }
-  }
 end
